4a55c5af56f79df1374b9ccfe14e1d40afc3a8ca
kitri <kitri@gmail.com>
2023-07-03 12:52:55 +0900

    initial commit

---
+++ src/test/java/kr/re/kitri/spring2023/Spring2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.re.kitri.spring2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
