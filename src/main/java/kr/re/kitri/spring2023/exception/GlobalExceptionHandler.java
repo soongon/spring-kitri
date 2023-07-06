11e98c545c6b9b9a9204b42890ea49e9ef0ed490
kitri <kitri@gmail.com>
2023-07-06 10:28:38 +0900

    initial commit

--- src/main/java/kr/re/kitri/spring2023/exception/GlobalExceptionHandler.java
+++ src/main/java/kr/re/kitri/spring2023/exception/GlobalExceptionHandler.java
@@ -1,8 +1,11 @@
 package kr.re.kitri.spring2023.exception;
 
+import kr.re.kitri.spring2023.model.ErrorMessage;
+import org.springframework.http.HttpStatus;
 import org.springframework.jdbc.BadSqlGrammarException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @ControllerAdvice // 모든 컨트롤러에 대한 예외처리를 수행하는 클래스임을 알려줌
@@ -10,7 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = BadSqlGrammarException.class)
-    public String handleBadSqlGrammarException(BadSqlGrammarException e) {
-        return "잘못된 SQL 문법입니다.";
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ErrorMessage handleBadSqlGrammarException(BadSqlGrammarException e) {
+        return new ErrorMessage("DB-001", "데이터베이스 오류가 발생했습니다.");
     }
 }
