776539bb22cd800fd600cdbb95cec527c3469e31
kitri <kitri@gmail.com>
2023-07-06 14:16:48 +0900

    initial commit

--- src/main/java/kr/re/kitri/spring2023/service/PostService.java
+++ src/main/java/kr/re/kitri/spring2023/service/PostService.java
@@ -4,10 +4,13 @@ import kr.re.kitri.spring2023.dao.PostRepository;
 import kr.re.kitri.spring2023.model.Post;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional // 이 클래스의 모든 메서드는 트랜잭션 처리를 해줘..
 public class PostService {
 
     @Autowired
@@ -26,6 +29,7 @@ public class PostService {
         return post;
     }
 
+    @Transactional(propagation = Propagation.NOT_SUPPORTED) // 트랜잭션을 지원하지 않는다.
     public Post modifyPost(Post post) {
         postRepository.updatePost(post);
         return post;
